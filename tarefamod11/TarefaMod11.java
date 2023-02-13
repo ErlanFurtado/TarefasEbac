b00b79d13816979ab95bd75d135dd74f84ace55b
Erlan Charles Furtado <erlanfurtado7@gmail.com>
2023-02-13 13:58:41 -0300

    tarefamod11

---
+++ tarefamod11/TarefaMod11.java
@@ -0,0 +1,16 @@
+package tarefamod11;
+
+import java.util.Scanner;
+
+public class TarefaMod11 {
+    public static void main(String[] args) {
+        Scanner leitorNome = new Scanner(System.in);
+        System.out.println("Digite os nomes e separe com virgula: ");
+        String input = leitorNome.nextLine();
+
+        String[] nomes = input.split(",");
+        System.out.println("Nomes: " + String.join(", ", nomes));
+
+    }
+
+}
